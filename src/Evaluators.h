c7a191caa654688df3f8560763e901613ce45b40
Michelle <kojsmn@miamioh.edu>
2015-10-27 02:58:38 +0000

    added evaluation functions - need to optimize code and write methods - also, need to change the way the return values for the eval function are used. Maybe should be EvalReserve - EvalTakeAway. EvalTakeAway counts how many reserve spots the opponent has and EvalReserve counts how many reserved spots home has.

--- src/Evaluators.h
+++ src/Evaluators.h
@@ -17,7 +17,46 @@ using DS = DomineeringState;
 
 struct EvalTakeAway {
     double operator()(const DS& state) const {
-        return u_dist(rd);
+	int count = 0;
+
+//	Either implement so it counts how many reserved are for opponent
+//	or counts how many spaces it takes away from opponent
+	// Counts how many reserved are for opponent
+	 for (int i = 0; i < state.ROWS; i++){
+                for (int j = 0; j < state.COLS; j++){
+                        if (state.getCurPlayerSym() == 'B' && state.getCell(i,j) == state.EMPTYSYM){
+                                if ((((j+1) < state.ROWS) && (state.getCell(i,j+1) == state.EMPTYSYM))){
+                                         if (((i-1) > 0) && ((j+1) > 0) && (state.getCell(i-1,j) != state.EMPTYSYM) && (state.getCell(i-1,j+1) != state.EMPTYSYM) && (((i+1) > state.ROWS) || ((j+1) > state.COLS)) || (((i+1) < state.ROWS) && ((j+1) < state.COLS) && (state.getCell(i+1,j) != state.EMPTYSYM) && (state.getCell(i+1,j+1) != state.EMPTYSYM)&& (((i-1) < 0) || ((j+1 < 0))))){
+
+                                                count++;
+                                          }
+                                          else if ((((i-1) > 0) && ((j+1) > 0) && (state.getCell(i-1,j) != state.EMPTYSYM) && (state.getCell(i-1,j+1) != state.EMPTYSYM)) && (((i+1) < state.ROWS) && ((j+1) < state.COLS) && (state.getCell(i+1,j) != state.EMPTYSYM) && (state.getCell(i+1,j+1) != state.EMPTYSYM))){
+                                                count++;
+                                          }
+                                 }
+                          }
+                          else if (state.getCurPlayerSym() == 'W' && (state.getCell(i,j) == state.EMPTYSYM)){
+                                         // See if adjacent square is open
+                                if ((((i+1) < state.ROWS) && (state.getCell(i+1,j) == state.EMPTYSYM))){
+                                         if ((((i+1) > 0) && ((j-1) > 0) && (state.getCell(i,j-1) != state.EMPTYSYM) && (state.getCell(i+1,j-1) != state.EMPTYSYM) && (((i+1) > state.ROWS) || ((j+1) > state.COLS))) || (((i+1) < state.ROWS) && ((j+1) < state.COLS) && (state.getCell(i,j+1) != state.EMPTYSYM) && (state.getCell(i+1,j+1) != state.EMPTYSYM)&& (((i-1) < 0) || ((j-1 < 0))))){
+
+                                                count++;
+                                          }
+                                          else if ((((i+1) > 0) && ((j-1) > 0) && (state.getCell(i,j-1) != state.EMPTYSYM) && (state.getCell(i+1,j-1) != state.EMPTYSYM)) && (((i+1) < state.ROWS) && ((j+1) < state.COLS) && (state.getCell(i,j+1) != state.EMPTYSYM) && (state.getCell(i+1,j+1) != state.EMPTYSYM))){
+                                                count++;
+                                           }
+                                }
+                          }
+                   }
+        }
+	
+
+	// Counts how many spaces it takes away from opponent
+
+	
+
+	return count;
+       // return u_dist(rd);
     }
 };
 
