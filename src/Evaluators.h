b4deb79791e397b6c11e03ff234d0fcc94ca6bb5
Michelle <kojsmn@miamioh.edu>
2015-10-27 01:07:51 +0000

    checking to see if adjacent square is empty - works

--- src/Evaluators.h
+++ src/Evaluators.h
@@ -23,14 +23,24 @@ struct EvalTakeAway {
 
 struct EvalReserve {
     double operator()(const DS& state) const {
-	std::cout << "HERE!" << std::endl;
 
-	for (int i = 0; i < state.ROWS; i++){
-		for (int j = 0; j < state.COLS; j++){
-			std::cout << state.getCell(i, j);
-		}
-			std::cout << std::endl;
-	}
+	std::cout << "HERE!" << std::endl;
+        std::cout << state.getCurPlayerSym() << std::endl;
+
+        int count = 0;
+
+        // Loop through every square in the board
+        for (int i = 0; i < state.ROWS; i++){
+                for (int j = 0; j < state.COLS; j++){
+                        std::cout << state.getCell(i, j);
+                        // See if adjacent square is open
+                        if ((i < state.ROWS) && (state.getCell(i,j) == state.EMPTYSYM)){
+                                std::cout << "EMPTY" << std::endl;
+                        }
+
+                }
+                        std::cout << std::endl;
+        }
         return u_dist(rd);
     }
 };
