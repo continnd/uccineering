99194db9d028e131378a80a3dc829ebd8547d542
Naoki Mizuno <nigorojr@gmail.com>
2015-11-03 00:27:31 -0500

    Rework evaluation function
    This commit makes the change of reorganizing the methods in the Evaluator.
    This also fixes the behavior when there are three consecutive grids, the
    program scores as 2 when there could only be one tile places.
    
    e.g.
    |   |   | A |
    +---+---+---+
    | H | H | A |
    +---+---+---+
    | 1 | 2 | 3 |
    +---+---+---+
    | A | H | H |
    +---+---+---+
    | A |   |   |
    
    This commit counts 1, 2, and 3 as score of 1.

--- src/Evaluators.h
+++ src/Evaluators.h
@@ -12,67 +12,80 @@ struct Evaluator {
     using score_t = int;
 
     /**
-     * Checks if there is space to put a `self' domino when r1 and c1 are the
-     * given.
-     * Depending on `self', bottom tile or the right tile is checked.
+     * Checks if the given grid is empty or not.
+     * Note that this method does boundary checks first, which returns false
+     * if any of the given parameter is invalid.
      */
-    bool has_space_for(const Who self, const int r1, const int c1,
-                       const DS& state) const {
-        if (state.getCell(r1, c1) != state.EMPTYSYM) {
+    inline bool grid_empty(const int r, const int c, const DS& state) const {
+        if (r < 0 || r >= state.ROWS || c < 0 || c >= state.COLS) {
             return false;
         }
 
-        auto r2 = self == Who::HOME ? r1 : r1 + 1;
-        auto c2 = self == Who::HOME ? c1 + 1 : c1;
+        return state.getCell(r, c) == state.EMPTYSYM;
+    }
 
-        if ((r1 < 0 || r1 >= state.ROWS || c1 < 0 || c1 >= state.COLS)
-            || (r2 < 0 || r2 >= state.ROWS || c2 < 0 || c2 >= state.COLS)) {
+    /**
+     * Checks if a tile is placable on the given location.
+     * A tile (i.e. domino) is placable if both grids are empty. Even though
+     * this method does boundary checking, it does not check if the two grids
+     * are adjacent to each other.
+     */
+    bool placable(const int r1, const int c1, const int r2, const int c2,
+                  const DS& state) const {
+        return grid_empty(r1, c1, state) && grid_empty(r2, c2, state);
+    }
+
+    /**
+     * Checks if HOME has a reserved spot.
+     * Home places dominoes horizontally. For example,
+     *
+     * +---+---+---+---+---+
+     * | H | H | 1 | 2 |
+     * +---+---+---+---+---+
+     * |   |   | A | A |
+     *
+     * When 1 and 2 are (r1, c1) and (r2, c2) respectively, this method
+     * returns true since the spot is "reserved" for HOME.
+     */
+    bool reserved_for_home(const int r1,
+                           const int c1,
+                           const int r2,
+                           const int c2,
+                           const DS& state) const {
+        // Check if 1, 2 (in the docs) are actually empty
+        if (!placable(r1, c1, r2, c2, state)) {
             return false;
         }
 
-        return state.getCell(r2, c2) == state.EMPTYSYM;
+        bool no_space_above = (!grid_empty(r1 + 1, c1, state) &&
+                               !grid_empty(r2 + 1, c2, state));
+
+        bool no_space_below = (!grid_empty(r1 - 1, c1, state) &&
+                               !grid_empty(r2 - 1, c2, state));
+
+        return no_space_above && no_space_below;
     }
 
-    bool home_edge(int r1, int c1, int c2, int r2, const DS& state) const {
-        return (r1 > 0 && r2 == state.ROWS
-                && c1 < state.COLS
-                && state.getCell(r1, c2) != state.EMPTYSYM
-                && state.getCell(r1, c1) != state.EMPTYSYM)
+    /**
+     * Checks if the given grid is reserved for AWAY.
+     * Away places vertical dominoes.
+     */
+    bool reserved_for_away(const int r1,
+                           const int c1,
+                           const int r2,
+                           const int c2,
+                           const DS& state) const {
+        if (!placable(r1, c1, r2, c2, state)) {
+            return false;
+        }
 
-            || (r1 < 0 && r2 < state.ROWS
-                && c1 < state.COLS
-                && state.getCell(r2, c2) != state.EMPTYSYM
-                && state.getCell(r2, c1) != state.EMPTYSYM);
-    }
+        bool no_space_to_left = (!grid_empty(r1, c1 - 1, state) &&
+                                 !grid_empty(r2, c2 - 1, state));
 
-    bool home_mid(int r1, int c1, int r2, int c2, const DS& state) const {
-        return (r1 > 0 && c1 > 0
-                && state.getCell(r1, c2) != state.EMPTYSYM
-                && state.getCell(r1, c1) != state.EMPTYSYM)
+        bool no_space_to_right = (!grid_empty(r1, c1 + 1, state) &&
+                                  !grid_empty(r2, c2 + 1, state));
 
-            && (r2 < state.ROWS && c1 < state.COLS
-                && state.getCell(r2, c2) != state.EMPTYSYM
-                && state.getCell(r2, c1) != state.EMPTYSYM);
-    }
-
-    bool away_edge(int r1, int c1, int r2, int c2, const DS& state) const {
-        return (c1 > 0 && c2 == state.COLS && r1 < state.ROWS
-                && state.getCell(r2, c1) != state.EMPTYSYM
-                && state.getCell(r1, c1) != state.EMPTYSYM)
-
-            || (c1 < 0 && c2 < state.COLS && r1 < state.ROWS
-                && state.getCell(r2, c2) != state.EMPTYSYM
-                && state.getCell(r1, c2) != state.EMPTYSYM);
-    }
-
-    bool away_mid(int r1, int c1, int r2, int c2, const DS& state) const {
-        return (r1 > 0 && c1 > 0
-                && r1 < state.ROWS
-                && c2 < state.COLS
-                && state.getCell(r2, c1) != state.EMPTYSYM
-                && state.getCell(r1, c1) != state.EMPTYSYM
-                && state.getCell(r2, c2) != state.EMPTYSYM
-                && state.getCell(r1, c2) != state.EMPTYSYM);
+        return no_space_to_left && no_space_to_right;
     }
 };
 
@@ -81,42 +94,30 @@ struct EvalReserve : public Evaluator {
         score_t home_count = 0;
         score_t away_count = 0;
 
-        for (int i = 0; i < state.ROWS; i++) {
-            for (int j = 0; j < state.COLS; j++) {
+        for (int r = 0; r < state.ROWS; r++) {
+            for (int c = 0; c < state.COLS; c++) {
                 // Check if HOME has reserved spot here
-                if (has_space_for(Who::HOME, i, j, state)
-                    && (home_edge(i - 1, j + 1, i + 1, j, state)
-                        || home_mid(i - 1, j + 1, i + 1, j, state))) {
+                // Note: the method does boundary check
+                if (reserved_for_home(r, c, r, c + 1, state)) {
                     home_count++;
+                    // Go to the next column to skip over (r, c + 1)
+                    c++;
                 }
-
+            }
+        }
+        for (int c = 0; c < state.COLS; c++) {
+            for (int r = 0; r < state.ROWS; r++) {
                 // Check if AWAY has reserved spot here
-                if (has_space_for(Who::AWAY, i, j, state)
-                    && (away_edge(i + 1, j - 1, i, j + 1, state)
-                        || away_mid(i + 1, j - 1, i, j + 1, state))) {
+                // Note: the method does boundary check
+                if (reserved_for_away(r, c, r + 1, c, state)) {
                     away_count++;
+                    // Go to the next row to skip over (r + 1, c)
+                    r++;
                 }
             }
         }
-	
-	 for (int i = 0; i < state.ROWS; i++) {
-            for (int j = 0; j < state.COLS; j++) {
-                std::cout << state.getCell(i,j);
 
-            }
-                std::cout << std::endl;
-        }
-
-
-        std::cout << "HOME: " << home_count << std::endl;
-
-        std::cout << "AWAY: " << away_count << std::endl;
-
-        // if we are home return this.
         return home_count - away_count;
-        // if we are away return this
-//      return away_count - home_count;
-
     }
 };
 
